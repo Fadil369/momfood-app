1f527c64c3a7d15e2de2bc3b91feba5c4d92e744
MOHAMED ABUAGLA <121701645+Fadil369@users.noreply.github.com>
2026-05-21 03:57:26 +0300

    feat: set ZuZu AI assistant as main application component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { LandingPage } from './pages/LandingPage'
+import ZuZuAgent from './ai/ZuZuAgent'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<ZuZuAgent />} />
       </Routes>
     </Router>
   )
